abb00b52de6d0ac93e4039dedc9f9cf4e082c30e
Mike-Skvortsov <mixaylo.skv@gmail.com>
2022-06-15 16:35:39 +0300

    lesson 17-18 Collections

---
+++ Lesson17.Collection/Lesson17.Collection/LinkedList.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Lesson17.Collections
+{
+    public class LinkedListEnumerator<T> : IEnumerator<T>
+    {
+        private readonly LinkedList<T> _list;
+        private int _index;
+
+        public LinkedListEnumerator(LinkedList<T> list)
+        {
+            this._list = list;
+        }
+
+        public bool MoveNext()
+        {
+            if (this._index < this._list.Count)
+            {
+                Current = this._list.GetByIndex(this._index++);
+                return true;
+            }
+
+            return false;
+        }
+
+        public T Current { get; private set; }
+
+        public void Reset()
+        {
+            this._index = 0;
+        }
+
+        object IEnumerator.Current { get; }
+
+        public void Dispose()
+        {
+        }
+    }
+
+    public class LinkedList<T> : ICollection<T>
+    {
+        private class Node<T>
+        {
+            public Node(T value, Node<T> next)
+            {
+                this.Value = value;
+                this.Next = next;
+            }
+
+            public T Value { get; }
+
+            public Node<T> Next { get; set; }
+        }
+
+        private Node<T> _head;
+        private Node<T> _tail;
+        private int _count;
+
+        public void Add(T item)
+        {
+            if (this._head == null)
+            {
+                this._head = new Node<T>(item, null);
+                this._tail = this._head;
+            }
+            else
+            {
+                var newNode = new Node<T>(item, null);
+                this._tail.Next = newNode;
+
+                this._tail = newNode;
+            }
+
+            this._count++;
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            var item = this._head;
+            while (item != null)
+            {
+                yield return item.Value;
+                item = item.Next;
+            }
+        }
+
+        public bool Contains(T item)
+        {
+            var pointer = this._head;
+            while (pointer != null)
+            {
+                if (pointer.Value.Equals(item))
+                {
+                    return true;
+                }
+                pointer = pointer.Next;
+            }
+
+            return false;
+        }
+
+        public T GetByIndex(int index)
+        {
+            if (index < 0 || index >= this._count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            var pointer = this._head;
+            for (int i = 0; i < index; i++)
+            {
+                pointer = pointer.Next;
+            }
+
+            return pointer.Value;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+            => throw new NotImplementedException();
+
+        public int Count => this._count;
+
+        public bool IsReadOnly { get; } = false;
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+            => new LinkedListEnumerator<T>(this);
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => this.GetEnumerator();
+    }
+}
---
+++ Lesson17.Collection/Lesson17.Collection/Set.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Lesson17.Collections
+{
+    public class Set<T> : ICollection<T>
+    {
+        private readonly LinkedList<T>[] _array;
+        private readonly int _capacity;
+
+        public Set(int capacity)
+        {
+            this._array = new LinkedList<T>[capacity];
+            this._capacity = capacity;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+            => throw new System.NotImplementedException();
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => this.GetEnumerator();
+
+        public void Add(T item)
+        {
+            if (item == null)
+            {
+                Console.WriteLine("Item is null");
+            }
+            else
+            {
+                var index = item.GetHashCode() % this._capacity;
+                var list = this._array[index] ?? new LinkedList<T>();
+
+                list.Add(item);
+                this._array[index] = list;
+            }
+        }
+
+        public void Clear()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException();
+            }
+            else
+            {
+                int index = item.GetHashCode() % this._capacity; // 5
+                LinkedList<T> list = this._array[index]; // getting a 5th list of array
+
+                if (list == null)
+                {
+                    return false;
+                }
+
+                return list.Contains(item); // if item in a 5th list
+            }
+
+            return false;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Remove(T item)
+            => throw new System.NotImplementedException();
+
+        public int Count { get; }
+
+        public bool IsReadOnly { get; }
+    }
+}
