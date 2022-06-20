9b94aa79c421df2273069c4f801a7c11427a2c20
Mike-Skvortsov <mixaylo.skv@gmail.com>
2022-06-20 15:51:14 +0300

    Homework 18-19 Snake

---
+++ Lesson18-19.Snake/Lesson18-19.Snake/Pixel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Lesson18_19.Snake
+{
+	public struct Pixel
+	{
+		private const char Border = '█';
+		public Pixel(int x, int y, ConsoleColor color, int pixelSize = 3)
+		{
+			this.X = x; 
+			this.Y = y;
+			this.Color = color;
+			this.PixelSize = pixelSize;
+		}
+		public int PixelSize { get; }
+		public int X { get; }
+		public int Y { get; }
+		public ConsoleColor Color { get; }
+		public void Draw()
+		{
+			Console.ForegroundColor = Color;
+			for (int x = 0; x < PixelSize; x++)
+			{
+				for (int y = 0; y < PixelSize; y++)
+				{
+					Console.SetCursorPosition(X * PixelSize + x, Y * PixelSize + y);
+					Console.Write(Border);
+				}
+			}
+		}
+		public void Clear()
+		{
+			for (int x = 0; x < PixelSize; x++)
+			{
+				for (int y = 0; y < PixelSize; y++)
+				{
+					Console.SetCursorPosition(X * PixelSize + x, Y * PixelSize + y);
+					Console.Write(" ");
+				}
+			}
+		}
+	}
+}
---
+++ Lesson18-19.Snake/Lesson18-19.Snake/Snake.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lesson18_19.Snake
+{
+	class Snake
+	{
+		public Queue<Pixel> BodySnake { get; } = new Queue<Pixel>();
+		public ConsoleColor HeadColor { get; }
+		public ConsoleColor BodyColor { get; }
+		public Pixel Head { get; set; }
+		public Snake(int headX, int headY, ConsoleColor headColor, ConsoleColor bodyColor, int bodyLength = 4)
+		{
+			this.HeadColor = headColor;
+			this.BodyColor = bodyColor;
+
+			this.Head = new Pixel(headX, headY, HeadColor);
+			for (int i = bodyLength; i >= 0; i--)
+			{
+				BodySnake.Enqueue(new Pixel(Head.X - i - 1, headY, BodyColor));
+			}
+			Draw();
+		}
+
+		public void Move(Direction direction, bool eat = false)
+		{
+			Clear();
+
+			BodySnake.Enqueue(new Pixel(Head.X, Head.Y, BodyColor));
+			if (!eat)
+				BodySnake.Dequeue();
+
+
+			Head = direction switch
+			{
+				Direction.Right => new Pixel(Head.X + 1, Head.Y, HeadColor),
+				Direction.Left => new Pixel(Head.X - 1, Head.Y, HeadColor),
+				Direction.Down => new Pixel(Head.X, Head.Y + 1, HeadColor),
+				Direction.Up => new Pixel(Head.X, Head.Y - 1, HeadColor),
+				_ => Head
+			};
+			Draw();
+		}
+		public void Draw()
+		{
+			Head.Draw();
+			foreach (var pixel in BodySnake)
+			{
+				pixel.Draw();
+			}
+		}
+		public void Clear()
+		{
+			Head.Clear();
+			foreach (var pixel in BodySnake)
+			{
+				pixel.Clear();
+			}
+		}
+	}
+}
