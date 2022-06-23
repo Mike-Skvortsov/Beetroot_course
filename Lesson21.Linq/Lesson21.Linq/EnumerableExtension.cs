0f66028c51a67ccbd6dcfb282264a1fce98d5520
Mike-Skvortsov <mixaylo.skv@gmail.com>
2022-06-23 12:05:38 +0300

    Lesson 21 Linq

---
+++ Lesson21.Linq/Lesson21.Linq/EnumerableExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson21.Linq
+{
+	public static class EnumerableExtension
+	{
+		public static IEnumerable<U> OwnSelect<T, U>(this IEnumerable<T> enumerable, Func<T, U> func)
+		{
+			foreach (var item in enumerable)
+			{
+				yield return func(item);
+			}
+		}
+		public static IEnumerable<T> OwnWhere<T>(this IEnumerable<T> enumerable,Func<T, bool> func)
+		{
+			foreach (var item in enumerable)
+			{
+				if (func(item))
+					yield return item;
+			}
+		}
+	}
+}
---
+++ Lesson21.Linq/Lesson21.Linq/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace Lesson21.Linq
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var array = Enumerable.Range(0, 20).ToArray();
+			var newArray = array.Where(x => x % 2 == 0).ToArray();
+			foreach (var item in newArray)
+			{
+				Console.WriteLine(item);
+			}
+			foreach (var item in array.Select(x => x * 2))
+			{
+				Console.WriteLine(item);
+			}
+
+			var enumerable = from item in array
+							 where item % 2 == 0
+							 select item * 3;
+
+			foreach (var item in array.Select(x => x * 2))
+			{
+				Console.WriteLine(item);
+			}
+			foreach (var item in array.OwnSelect(x => x * 2))
+			{
+				Console.WriteLine(item);
+			}
+			array.Any(x => x == 100);
+			array.All(x => x < 100);
+			array.Take(3);
+
+			foreach (var item in array.Append(40).Append(50))
+			{
+				Console.WriteLine(item);
+			}
+			var balls = Enumerable.Range(0, 10).Select(x => new Ball());
+			balls.Where(x => x.Color == Color.Green).Select(x => x.Color = Color.Blue).Take(2);
+		}
+		class Ball
+		{
+			public Color Color { get; set; }
+		}
+		enum Color
+		{
+			Green = 1,
+			Blue = 2
+		}
+	}
+}
