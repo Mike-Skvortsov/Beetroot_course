d39b4e1c62b4252f57af6d30cceb01a09fb5335a
Mike-Skvortsov <mixaylo.skv@gmail.com>
2022-07-03 20:46:57 +0300

    classwork lesson 23 dependencies

---
+++ Lesson23.Dependencies/Lesson23.DataAccess/FileDataAccess.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Lesson23.Contracts;
+
+namespace Lesson23.DataAccess
+{
+    public interface IDataAccess
+    {
+        List<Room> GetAll();
+        void WriteAll(List<Room> rooms);
+    }
+
+    public class FileDataAccess : IDataAccess
+    {
+        private string filePath = "rooms.file";
+
+        public List<Room> GetAll()
+        {
+            using (Stream stream = File.Open(this.filePath, FileMode.Open))
+            {
+                var binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                return (List<Room>)binaryFormatter.Deserialize(stream);
+            }
+        }
+
+        public void WriteAll(List<Room> rooms)
+        {
+            using (Stream stream = File.Open(this.filePath, FileMode.Append))
+            {
+                var binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                binaryFormatter.Serialize(stream, rooms);
+            }
+        }
+    }
+}
---
+++ Lesson23.Dependencies/Lesson23.Domain/RoomService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lesson23.Contracts;
+using Lesson23.DataAccess;
+
+namespace Lesson23.Domain
+{
+
+	public class RoomService
+	{
+		private FileDataAccess _fileDataAccess;
+		public RoomService()
+		{
+			this._fileDataAccess = new FileDataAccess();
+		}
+
+		public List<Room> GetAll()
+		{
+			return this._fileDataAccess.GetAll();
+		}
+		public void WriteAll(List<Room> rooms)
+		{
+			this._fileDataAccess.WriteAll(rooms);
+		}
+	}
+}
