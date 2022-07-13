f015af80862eaef49ff78ded5f36792d457215c6
Mike-Skvortsov <mixaylo.skv@gmail.com>
2022-07-13 21:43:21 +0300

    homework lesson 26 SQL

---
+++ Lesson25.SQL/homework lesson 26 SQL.sql
@@ -0,0 +1,14 @@
+select * from Persons where Gender = 'M'
+
+select * from Persons where Age = 31
+
+select * from Persons where Age > 18
+
+update Persons set Age += 1
+
+delete from Persons where Age > 30
+
+select count(*) rows from Persons
+
+select count(*) as count from Persons
+group by Age	
