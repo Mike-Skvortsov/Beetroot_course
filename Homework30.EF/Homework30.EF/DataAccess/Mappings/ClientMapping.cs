87b0a4a0eaed6dc5fc39b60ebd592b039b359db8
Mike-Skvortsov <mixaylo.skv@gmail.com>
2022-07-29 15:39:29 +0300

    Homework EF

---
+++ Homework30.EF/Homework30.EF/DataAccess/Mappings/ClientMapping.cs
@@ -0,0 +1,16 @@
+﻿using Homework30.EF.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Homework30.EF.DataAccess.Mappings
+{
+	public class ClientMapping
+	{
+		public void Configure(EntityTypeBuilder<Clients> builder)
+		{
+			builder.ToTable("Clients", "dbo");
+
+			builder.HasKey(x => x.Id);
+		}
+	}
+}
---
+++ Homework30.EF/Homework30.EF/DataAccess/Mappings/ProductMapping.cs
@@ -0,0 +1,21 @@
+﻿using Homework30.EF.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Homework30.EF.DataAccess.Mapping
+{
+	public class ProductMapping : IEntityTypeConfiguration<Products>
+	{
+		public void Configure(EntityTypeBuilder<Products> builder)
+		{
+			builder.ToTable("Products", "dbo");
+
+			builder.HasKey(x => x.Id);
+
+			builder
+				.HasOne(x => x.Orders)
+				.WithMany(x => x.Products)
+				.HasForeignKey(x => x.OrderId);
+		}
+	}
+}
