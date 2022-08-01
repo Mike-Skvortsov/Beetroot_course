1aecbdd74b1e3403de87ffbcc47215395d844886
Mike-Skvortsov <mixaylo.skv@gmail.com>
2022-08-01 17:50:56 +0300

    homework lesson 35 ASP.Net

---
+++ homework35.ASP.Net.Core.2.0/homework35.ASP.Net.Core.2.0/Controllers/ProductController.cs
@@ -0,0 +1,67 @@
+﻿using homework35.ASP.Net.Core._2._0.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace homework35.ASP.Net.Core._2._0.Controllers
+{
+	[Route("[controller]")]
+	public class ProductController : Controller
+	{
+		[HttpGet("List")]
+		public IActionResult Index()
+		{
+			IEnumerable<ProductModel> products = Storage<ProductModel>.Instance.GetAll();
+
+			return View("List", products);
+		}
+
+		[HttpGet("create")]
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpPost("create")]
+		public IActionResult Create([FromForm] ProductModel model)
+		{
+			Storage<ProductModel>.Instance.Add(model);
+			return View(model);
+		}
+
+		[HttpPost("update/{id}")]
+		public IActionResult Update([FromForm] ProductModel model, [FromRoute] int id)
+		{
+			Storage<ProductModel>.Instance.Update(model, id);
+			return View(model);
+		}
+
+		[HttpGet("update/{id}")]
+		public IActionResult Update([FromRoute] int id)
+		{
+			var model = Storage<ProductModel>.Instance.Get(id);
+
+			ViewData["Title"] = model.Title;
+			ViewData["Price"] = model.Price;
+
+			return View();
+		}
+
+		[HttpPost("delete/{id}")]
+		public IActionResult Delete([FromRoute] int id)
+		{
+			Storage<ProductModel>.Instance.Delete(id);
+			return View();
+		}
+
+		[HttpGet("delete/{id}")]
+		public IActionResult DeleteGet([FromRoute] int id)
+		{
+			var model = Storage<ProductModel>.Instance.Get(id);
+
+			ViewData["Title"] = model.Title;
+			ViewData["Price"] = model.Price;
+
+			return View();
+		}
+	}
+}
---
+++ homework35.ASP.Net.Core.2.0/homework35.ASP.Net.Core.2.0/Storage.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace homework35.ASP.Net.Core._2._0
+{
+    public class Storage<T>
+    {
+        private static Storage<T> _instance;
+        private List<T> _data;
+        public static Storage<T> Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Storage<T>();
+                }
+                return _instance;
+            }
+        }
+
+        private Storage()
+        {
+            _data = new List<T>();
+        }
+
+        public IReadOnlyCollection<T> GetAll()
+        {
+            return _data;
+        }
+
+        public void Add(T item)
+        {
+            _data.Add(item);
+        }
+        public void Update(T item, int index) => _data[index] = item;
+        public void Delete(int id) => _data.RemoveAt(id);
+        public T Get(int index) => _data[index];
+    }
+}
